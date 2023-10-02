da83534058b341ed90caa46af104542eec1f91b9
winning godspower <winninggodspower@gmail.com>
2023-10-02 12:03:23 +0100

    modified register page

--- src/pages/Register/RegisterPatient.jsx
+++ src/pages/Register/RegisterPatient.jsx
@@ -1,9 +1,10 @@
+import { Form } from "react-router-dom";
 import checkIcon from "../../assets/check-icon.svg";
 
 function RegisterPatient() {
     return (
-        <main className="md:flex font-raleway">
-            <section id="side-bar" className="register-img hidden w-2/5 md:block text-white h-[550px]" >
+        <main className="md:flex font-raleway md:h-[calc(100vh-3rem)]">
+            <section id="side-bar" className="register-img hidden w-2/5 md:block text-white h-full" >
                 <div className="mt-[6rem] w-[80%] mx-auto ">
                     <h2 className="mb-8 text-4xl font-railway">Gain access to 11k+ patients from all over Africa</h2>
                     <ul className="mb-9 ">
@@ -24,27 +25,27 @@ function RegisterPatient() {
             <section className="grid w-full md:w-3/5 bg-grey">
                 <div className="w-full md:w-[65%] m-auto bg-white py-10">
                     
-                    <div className="grid items-center md:w-[23.3rem] max-w-[95%] mx-auto">
-                        <h3 className="font-raleway font-semibold text-center text-[2rem] opacity-90 mb-1">Patient register</h3>
-                        <p className="font-normal text-center opacity-90 font-raleway">And gain access to 400k worldwide medical specialists medical freedom</p>
+                    <div className="grid items-center w-10/12 md:w-[23.3rem] md:max-w-[90%] mx-auto">
+                        <h3 className="font-raleway font-semibold text-2xl sm:text-3xl text-center md:text-[2rem] opacity-90 mb-1">Patient register</h3>
+                        <p className="font-normal text-center text-base md:text-lg opacity-90 font-raleway">And gain access to 400k worldwide medical specialists medical freedom</p>
 
-                        <form className="flex flex-col items-center w-full mt-9 font-inter " action="" method="post">
+                        <Form className="flex flex-col items-center w-full mt-9 font-inter overflow-auto" action="" method="post">
                             
-                            <input className="w-full h-11 bg-grey-2 rounded-md border-[1px] mb-5 border-grey-3 " type="email" name="" id="" placeholder="Email Address" />
+                            <input className=" custom-input w-full h-11 bg-grey-2 rounded-md" type="email" name="" id="" placeholder="Email Address" />
 
-                            <div className=" max-w-full w-full grid grid-cols-[auto_1fr]">
-                                <select className=" bg-grey-4 rounded-md rounded-r-none border-r-0 border-[1px] mb-5 border-grey-3" name="" id="">
+                            <div className="grid grid-cols-[minmax(0,6.5rem)_minmax(0,1fr)] w-full group">
+                                <select className="custom-input group-focus-within:input-hover  bg-grey-4 rounded-md rounded-r-none border-r-0 " name="" id="">
                                     <option value="">+234</option>
                                     <option value="">+22</option>
                                     <option value="">+235</option>
                                 </select>
-                                <input className=" bg-grey-2 flex-grow rounded-md rounded-l-none border-l-0 border-[1px] mb-5 border-grey-3 " type="number" placeholder="Phone number" />
+                                <input className="custom-input group-focus-within:input-hover bg-grey-2 rounded-md rounded-l-none border-l-0" type="number" placeholder="Phone number" />
                             </div>
 
-                            <input className="w-full h-11 bg-grey-2 rounded-md border-[1px] mb-5 border-grey-3 " type="password" placeholder="Password" />
+                            <input className="custom-input w-full h-11 bg-grey-2 rounded-md " type="password" placeholder="Password" />
 
-                            <button className="w-full mt-5 rounded-md btn bg-secondary" type="submit">Register as a patient</button>
-                        </form>
+                            <button className="w-full md:w-[min(24.7rem, 100%)] mt-5 rounded-md btn bg-secondary" type="submit">Register as a patient</button>
+                        </Form>
 
                         <div className="mt-10">
                             <a className="block text-center text-grey-5" href="">Already have an account? Login</a>
