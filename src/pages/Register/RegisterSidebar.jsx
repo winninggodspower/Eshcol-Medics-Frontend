696c1451b20e16d580995e6033a0bc96e805d18d
winning godspower <winninggodspower@gmail.com>
2023-10-02 13:03:48 +0100

    added register patient page

---
+++ src/pages/Register/RegisterSidebar.jsx
@@ -0,0 +1,26 @@
+import checkIcon from "../../assets/check-icon.svg";
+
+function RegisterSidebar({points, btnColor}) {
+
+    // console.log(points);
+  return (
+    <section id="side-bar" className="register-img hidden w-2/5 md:block text-white h-full" >
+        <div className="mt-[6rem] w-[80%] mx-auto ">
+            <h2 className="mb-8 text-4xl font-railway">Gain access to 11k+ patients from all over Africa</h2>
+            <ul className="mb-9 ">
+                {
+                points.map((point, index)=>{
+                    return <li key={index} className="flex mb-3 gap-x-2 font-inter"><img src={checkIcon} alt="" />
+                        {point}
+                    </li>
+                })
+                }
+               
+            </ul>
+            <a className={`btn ${btnColor} rounded-md`} href="">Login as a patient</a>
+        </div>
+    </section>
+  )
+}
+
+export default RegisterSidebar;
