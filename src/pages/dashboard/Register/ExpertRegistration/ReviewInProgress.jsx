81162642c01b90ad01a46cfb768f44cea420f70b
winning godspower <winninggodspower@gmail.com>
2024-01-22 14:37:20 +0100

    feat: work on review in progress page

--- src/pages/dashboard/Register/ExpertRegistration/ReviewInProgress.jsx
+++ src/pages/dashboard/Register/ExpertRegistration/ReviewInProgress.jsx
@@ -5,10 +5,10 @@ function ReviewInProgress() {
     return (
         <section className="flex-grow flex md:items-center justify-center min-h-[calc(100vh-7.6rem)] md:min-h-[calc(100vh-4.65rem)]" >
             <div className='md:w-[36rem] max-w-[80%]'>
-                <div className=' pt-20 md:pt-9 flex flex-col items-center justify-center font-railway '>
+                <div className='flex flex-col items-center justify-center pt-20  md:pt-9 font-railway'>
                     <img src={reviewInProgressIllustration} alt="review illustration" />
-                    <h4 className='mt-10 mb-3 md:mb-4 font-semibold md:text-xl text-lg'>Review in progress</h4>
-                    <p className='text-center text-sm md:text-base'>We are taking a look at the information and documents you submitted
+                    <h4 className='mt-10 mb-3 text-lg font-semibold md:mb-4 md:text-xl'>Review in progress</h4>
+                    <p className='text-sm text-center md:text-base'>We are taking a look at the information and documents you submitted
                         We’ll send you an email if you meet the requirements we’re looking for</p>
                 </div>
             </div>
