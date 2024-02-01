289009ec2b322341a002f14f8da1cdd5d810d5c5
winning godspower <winninggodspower@gmail.com>
2024-02-01 10:19:09 +0100

    feat: work on services page

--- src/pages/dashboard/Services/index.jsx
+++ src/pages/dashboard/Services/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
+// services images
+import doctorImage from "../../../assets/services-image/doctor.png"
+import ambulanceImage from "../../../assets/services-image/ambulance.png"
+import nurseImage from "../../../assets/services-image/nurse.png"
+import billHistoryImage from "../../../assets/services-image/bill-history.png"
+import consultationImage from "../../../assets/services-image/consultation.png"
+import expertHistoryImage from "../../../assets/services-image/expert-history.png"
+
 function PatientServices() {
     return (
         <div className='grow'>
-            <div className='w-full max-w-[55rem] mx-auto '>
-                <div className='flex justify-between w-full py-9 px-3'>
+            <div className='w-full max-w-[55rem] mx-auto px-1'>
+                <div className='flex justify-around md:justify-between w-full py-9'>
                     <div></div>
                     <div className='flex items-center h-10 md:h-12 md:w-64 overflow-hidden border-[1px] border-grey-3 border-opacity-40 rounded-md ps-3 me-3 md:ps-4'>
                         <svg className='w-[17px] md:w-[24px]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
@@ -23,15 +31,47 @@ function PatientServices() {
                     </div>
                 </div>
 
-                <div id="services" >
-                    <h3>Services</h3>
+                <div id="services" className=' md:rounded-md text-white mb-10' style={{background: "linear-gradient(112deg, #5B7D6C 0.78%, #527362 99.62%)"}}>
 
-                    <div className="grid grid-cols-4 md:grid-cols-2">
-                        <div>
-                            <img src="" alt="" />
-                            <p>consult a doctor</p>
-                            <button>consult now</button>
+                    <h3 className='font-railway text-center text-2xl font-semibold md:font-bold pt-7'>Services</h3>
+
+                    <div className="grid md:grid-cols-3 grid-cols-2 md:gap-y-16 gap-y-[30px] justify-around font-inter pt-11 pb-12 md:w-[650px] max-w-[95%] mx-auto" >
+                        <div className='text-center justify-self-center'>
+                            <img className='mx-auto bg-white rounded-full' src={billHistoryImage} alt="" />
+                            <p className='my-4 text-sm font-medium'>consult a doctor</p>
+                            <button className='service-btn'>consult now</button>
                         </div>
+
+                        <div className='text-center justify-self-center'>
+                            <img className='mx-auto bg-white rounded-full' src={ambulanceImage} alt="" />
+                            <p className='my-4 text-sm font-medium'>Call an ambulance</p>
+                            <button className='service-btn'>Call now</button>
+                        </div>
+
+                        <div className='text-center justify-self-center'>
+                            <img className='mx-auto bg-white rounded-full' src={nurseImage} alt="" />
+                            <p className='my-4 text-sm font-medium'>Hire a nurse</p>
+                            <button className='service-btn'>Hire now</button>
+                        </div>
+
+                        <div className='text-center justify-self-center'>
+                            <img className='mx-auto bg-white rounded-full' src={doctorImage} alt="" />
+                            <p className='my-4 text-sm font-medium'>Bill history</p>
+                            <button className='service-btn'>Hire now</button>
+                        </div>
+
+                        <div className='text-center justify-self-center'>
+                            <img className='mx-auto bg-white rounded-full' src={consultationImage} alt="" />
+                            <p className='my-4 mt-3 text-sm font-medium'>Emergency consultation</p>
+                            <button className='service-btn'>Call now</button>
+                        </div>
+
+                        <div className='text-center justify-self-center'>
+                            <img className='mx-auto bg-white rounded-full' src={expertHistoryImage} alt="" />
+                            <p className='my-4 text-sm font-medium'>Expert history</p>
+                            <button className='service-btn'>Hire now</button>
+                        </div>
+
                     </div>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ export default {
         'secondary': '#352E43',
         'main': '#055C9F',
         'Green': '#079E16',
+        'Green-2': '#567867',
         'grey': '#7C8C93',
         'grey-2': 'rgba(217, 217, 217, 0.3)',
         'grey-3': 'rgba(0, 0, 0, 0.4)',
