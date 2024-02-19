3d02d40fa211ee77cb2ac458d2ce9dad869c7288
winning godspower <winninggodspower@gmail.com>
2024-02-19 13:28:06 +0100

    feat: make chatbox input sticky in mobile

--- src/pages/dashboard/Services/ConsultDoctor/SessionChat.jsx
+++ src/pages/dashboard/Services/ConsultDoctor/SessionChat.jsx
@@ -39,10 +39,10 @@ function SessionChat() {
           <ChatBubble sender={true} message={'I’m having blisters on my eyes'} />
         </div>
 
-        <div className="absolute bottom-0 w-full py-3 md:py-6 before:content-[''] before:block before:absolute before:bottom-0 before:start-0 before:w-full before:h-full before:bg-grey-3  before:opacity-70">
+        <div className="fixed md:absolute bottom-0 w-full py-3 md:py-6 before:content-[''] before:block before:absolute before:bottom-0 before:start-0 before:w-full before:h-full before:bg-grey-3  before:opacity-70">
           <form action="" className='w-[83%] mx-auto h-11 md:h-14 relative'>
             <input className='w-full rounded-3xl h-full bg-[rgba(108,108,108,.4)] border-0 md:ps-24 ps-5 text-black/50 font-inter' type="text" />
-            <button type='button' className="absolute left-0 w-14 md:w-20 h-full bg-tertiary rounded-3xl">
+            <button type='button' className="absolute left-0 h-full w-14 md:w-20 bg-tertiary rounded-3xl">
               <svg onClick={animate} className='mx-auto transition-transform ease-out' width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M15 6.25V23.75" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                 <path d="M6.25 15H23.75" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
