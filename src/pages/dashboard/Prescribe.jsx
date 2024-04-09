47dc9b7213000839cbf4b40da98f149fd504f145
winning godspower <winninggodspower@gmail.com>
2024-04-09 15:43:14 +0100

    feat: change presription modal background color

--- src/pages/dashboard/Prescribe.jsx
+++ src/pages/dashboard/Prescribe.jsx
@@ -35,7 +35,7 @@ export default Prescribe;
 
 function PrescribeModal({openModal, setOpenModal}) {
   return (
-    <div id="default-modal" tabIndex="-1" aria-hidden="true" className={ `${(openModal? 'flex': 'hidden')} overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-gray-500/50`}>
+    <div id="default-modal" tabIndex="-1" aria-hidden="true" className={ `${(openModal? 'flex': 'hidden')} overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black bg-opacity-60`}>
     <div className="relative p-4 w-full max-w-2xl max-h-full">
         {/* <!-- Modal content --> */}
         <div className="relative bg-white rounded-lg shadow ">
