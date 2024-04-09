af5a6aa059a2795a062bd8b023806f42a308a2df
winning godspower <winninggodspower@gmail.com>
2024-04-09 14:20:07 +0100

    feat: make prescribe page

---
+++ src/pages/dashboard/Prescribe.jsx
@@ -0,0 +1,17 @@
+import CustomPageHeader from "../../components/CustomPageHeader";
+
+
+// page where doctors use to prescribes drugs 
+function Prescribe() {
+    return (
+        <div className='h-full bg-white grow md:border-e-4'>
+            <CustomPageHeader title={'Prescriptions'} />
+
+            <div className="text-base md:text-lg leading-[30px] font-inter font-medium mx-9 md:mx-10" >
+                
+            </div>
+        </div>
+    )
+}
+
+export default Prescribe
